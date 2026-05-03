1a75ebe51965d663eb2ff16ba769d425c4ff285d
mfaria0110 <mfaria1965@icloud.com>
2026-05-03 00:41:51 -0300

    fix home api + rotas

--- public/js/cadastro.js
+++ public/js/cadastro.js
@@ -91,7 +91,7 @@ document.addEventListener("keydown", (e) => {
 });
 
 document.getElementById("btnVoltar").onclick = () => {
-  window.location = "/public/home.html";
+  window.location = "/home.html";
 };
 
 
