7c37b0b43e243cf05d311312ed3eaa36e19083fe
mfaria0110 <mfaria1965@icloud.com>
2026-05-23 08:21:02 -0300

    fix marker leaflet

--- src/components/MapPicker.jsx
+++ src/components/MapPicker.jsx
@@ -1,3 +1,5 @@
+import L from "leaflet"
+
 import {
   MapContainer,
   TileLayer,
@@ -6,6 +8,20 @@ import {
 } from "react-leaflet"
 
 import "leaflet/dist/leaflet.css"
+
+delete L.Icon.Default.prototype._getIconUrl
+
+L.Icon.Default.mergeOptions({
+
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+
+  iconUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+
+  shadowUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png"
+})
 
 export default function MapPicker({
   lat,
