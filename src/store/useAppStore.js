833f97351e7ef03df560178f7f70df87c6430d02
mfaria0110 <mfaria1965@icloud.com>
2026-05-26 12:22:20 -0300

    Corrige semPlano quando query não executa

--- src/store/useAppStore.js
+++ src/store/useAppStore.js
@@ -179,28 +179,25 @@ export const useAppStore =
         "perfil"
       )
 
-set({
-  accessToken:
-    data.accessToken,
+      set({
+        accessToken:
+          data.accessToken,
 
-  refreshToken:
-    data.refreshToken,
+        refreshToken:
+          data.refreshToken,
 
-  usuario:
-    data.usuario,
+        usuario:
+          data.usuario,
 
-  lojas:
-    data.lojas || [],
+        lojas:
+          data.lojas || [],
 
-  permissoes:
-    data.permissoes || [],
+        permissoes:
+          data.permissoes || [],
 
-  lojaId:
-    lojaInicial,
-
-  perfil: null
-})
-      
+        lojaId: null,
+        perfil: null
+      })
     },
 
     setAccessToken: (
